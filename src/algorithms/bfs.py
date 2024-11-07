c23bafa4ce51656e93b0f80add1dbe65e0a7fc05
dirii <minhquang116@gmail.com>
2024-11-07 11:06:07 +0700

    Add AStar, BFS, DFS, and UCS algorithms

---
+++ src/algorithms/bfs.py
@@ -0,0 +1,15 @@
+from base_algo import BaseAlgo
+from ..map_data import MapData
+
+class BFS(BaseAlgo):
+    def __init__(self):
+        self.map = None
+
+    def set_map(self, map: MapData):
+        self.map = map
+
+    def run(self) -> bool:
+        pass
+
+    def get_map(self) -> MapData:
+        pass
